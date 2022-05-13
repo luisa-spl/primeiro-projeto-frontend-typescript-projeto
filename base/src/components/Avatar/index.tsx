b2d3a02bdd4223372c2ed6ce36171239f0f9d3f8
luisa-spl <luisa.spinola9@gmail.com>
2022-05-13 19:38:29 -0300

    feat: avatar component

---
+++ base/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+type Props = {
+    image: string;
+    size?: number;
+}
+
+function Avatar({ image, size }: Props) {
+
+    const sizeAvatar = size || 147;
+
+    return (
+        <img src={image} alt="avatar" className='avatar' style={{ width: `${sizeAvatar}px`, height: `${sizeAvatar}px`}}/>
+    )
+}
+
+export default Avatar;
